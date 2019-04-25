5cd1ead834de9a234fb1275207a37a9d6f269523
minhkd98 <34558282+minhkd98@users.noreply.github.com>
2019-04-25 10:39:36 +0700

    Update Database/onlinekanbandtb.sql

--- Database/onlinekanbandtb.sql
+++ Database/onlinekanbandtb.sql
@@ -15,20 +15,20 @@ USE `kanbanproj`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-/*Table structure for table `kanbanonline` */
+/*Table structure for table `users` */
 
-DROP TABLE IF EXISTS `kanbanonline`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `kanbanonline` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-/*Data for the table `kanbanonline` */
+/*Data for the table `users` */
 
-insert  into `kanbanonline`(`id`,`username`,`pass`) values (1,'minhdtr','12345678');
+insert  into `users`(`id`,`username`,`pass`) values (1,'minhdtr','12345678');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
